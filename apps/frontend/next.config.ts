98f4321d91fd6fd4db5f72d8364f6d736fb824c6
Nylast-bit <stalyn.fernandez27@gmail.com>
2025-11-12 10:20:37 -0400

    updating the remotePatterns of images on next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,9 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "hcxhpldalsvjftgezvfu.supabase.co",
-        pathname: "/storage/v1/object/public/**",
+        protocol: 'http',
+        hostname: '216.245.184.30',
+        port: '', // Puerto 80, así que déjalo vacío
+        pathname: '/uploads/**', // Permite cualquier imagen dentro de /uploads/
       },
     ],
   },
